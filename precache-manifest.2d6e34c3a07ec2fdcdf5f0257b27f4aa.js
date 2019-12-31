ab5b1cf2dd19428b03b7029f0081b1f726edfaad
duanguanjun <duanguanjun@yeah.net>
2019-12-31 17:46:45 +0800

    Updates

---
+++ precache-manifest.2d6e34c3a07ec2fdcdf5f0257b27f4aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "884aef76bf1d8d5c9394ad6932983ec8",
+    "url": "/exchange/index.html"
+  },
+  {
+    "revision": "4f8106609cc533b2acfe",
+    "url": "/exchange/static/css/main.9fc4fdf3.chunk.css"
+  },
+  {
+    "revision": "a0603507f234df1bae5b",
+    "url": "/exchange/static/js/2.fc1fe354.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/exchange/static/js/2.fc1fe354.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4f8106609cc533b2acfe",
+    "url": "/exchange/static/js/main.44e717f0.chunk.js"
+  },
+  {
+    "revision": "fa1d058c94224a6f3346",
+    "url": "/exchange/static/js/runtime-main.a2aed6a3.js"
+  }
+]);
